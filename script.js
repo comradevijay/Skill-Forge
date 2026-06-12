d65c7a90fe54202a58d83bd2d54b0b823b53bb5e
ComradeVijay <comradevijay93@gmail.com>
2026-06-12 22:54:36 +0530

    added courses section

---
+++ script.js
@@ -0,0 +1,6 @@
+document.querySelectorAll('.filter-btn').forEach(btn => {
+  btn.addEventListener('click', function() {
+    document.querySelectorAll('.filter-btn').forEach(b => b.classList.remove('active'));
+    this.classList.add('active');
+  });
+});
